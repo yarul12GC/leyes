330c139d8da58dc75cbfbfd811a152317ccc49bf
yarul12GC <yahir634gallo@gmail.com>
2024-10-30 11:51:15 -0600

    se agregan cambios del anexo1

--- src/app/leyes/panel/panel.component.ts
+++ src/app/leyes/panel/panel.component.ts
@@ -12,34 +12,19 @@ export interface Curso {
   standalone: true,
   imports: [CommonModule],
   templateUrl: './panel.component.html',
-  styleUrls: ['./panel.component.css'] 
+  styleUrls: ['./panel.component.css']
 })
 export default class PanelComponent {
   cursos: Curso[] = [
     {
       titulo: 'Curso de Desarrollo Web',
       descripcion: 'Aprende las bases del desarrollo web con HTML, CSS y JavaScript.',
-      imagen: 'https://via.placeholder.com/400x200'
+      imagen: 'public/26.png'
     },
     {
       titulo: 'Curso de Marketing Digital',
       descripcion: 'Descubre cómo impulsar tu negocio en el mundo digital a través del marketing.',
       imagen: 'https://via.placeholder.com/400x200'
     },
-    {
-      titulo: 'Curso de Python para Data Science',
-      descripcion: 'Aprende Python y cómo aplicarlo en el campo del análisis de datos.',
-      imagen: 'https://via.placeholder.com/400x200'
-    },
-    {
-      titulo: 'Curso de Diseño Gráfico',
-      descripcion: 'Domina las herramientas clave para convertirte en un diseñador gráfico profesional.',
-      imagen: 'https://via.placeholder.com/400x200'
-    },
-    {
-      titulo: 'Curso de Inteligencia Artificial',
-      descripcion: 'Explora los conceptos y aplicaciones de la inteligencia artificial en la actualidad.',
-      imagen: 'https://via.placeholder.com/400x200'
-    }
   ];
 }
