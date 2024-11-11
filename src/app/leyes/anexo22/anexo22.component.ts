79e5424b589037bc64cbd823e71fac7000ccf755
yarul12GC <yahir634gallo@gmail.com>
2024-11-11 15:55:54 -0600

    se agregan cambios al anexo 22

--- src/app/leyes/anexo22/anexo22.component.ts
+++ src/app/leyes/anexo22/anexo22.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-anexo22',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './anexo22.component.html',
   styleUrl: './anexo22.component.css'
 })
 export default class Anexo22Component {
+  isMenuOpen = false;
 
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.fragment.subscribe(fragment => {
+      if (fragment) {
+        const element = document.getElementById(fragment);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth' });
+        }
+      }
+    });
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  toggleSubMenu(seccion: any) {
+    seccion.isOpen = !seccion.isOpen;
+  }
 }
